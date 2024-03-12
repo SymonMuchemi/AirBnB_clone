861e7d8392684add2471a2f1e1f877ebf27b6652
SymonMuchemi <simonkenya846@gmail.com>
2024-03-12 17:49:27 +0300

    console.py cleaned

--- console.py
+++ console.py
@@ -18,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
 
     list_of_classes = [
         "BaseModel", "USer", "Amenity",
-        "City", "State", "Place", "Review"        
+        "City", "State", "Place", "Review"
     ]
 
     missing_class = "** class name missing **"
@@ -103,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
             print(obj)
         else:
             print(self.no_instance)
-            
+
     def do_destroy(self, args):
         """deletes an instance based on the class name and id"""
         if not args.strip():
@@ -138,29 +138,29 @@ class HBNBCommand(cmd.Cmd):
 
         del instance_objs[key]
         storage.save()
-        
+
     def do_all(self, arg):
         """Prints all the instances saved"""
-        all_instances = storage.all()
+        instances = storage.all()
         args = arg.split()
-        
+
         if len(args) < 1:
-            print(["{}".format(str(v)) for _, v in all_instances.items()])
+            print(["{}".format(str(v)) for _, v in instances.items()])
             return
-        
+
         if args[0] not in self.list_of_classes:
             print(self.missing_class)
             return
-        
+
         else:
             print([f"{v}"]
-                  for _, v in all_instances.items() if type(v).__name__ == args[0])
+                  for _, v in instances.items() if type(v).__name__ == args[0])
             return
 
     def do_update(self, arg):
         """updates an instance"""
         args = arg.split(maxsplit=3)
-        
+
         if not args.strip():
             print(self.missing_class)
             return
@@ -179,7 +179,7 @@ class HBNBCommand(cmd.Cmd):
         if class_name not in self.list_of_classes:
             print(self.non_existant_class)
             return
-        
+
         instance_objs = storage.all()
         key = "{}.{}".format(args[0], args[1])
         req_instance = instance_objs.get(key, None)
@@ -199,7 +199,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(req_instance, k, v)
             storage.save()
             return
-        
+
         if len(args) < 3:
             print("** attribute name missing **")
             return
@@ -213,7 +213,7 @@ class HBNBCommand(cmd.Cmd):
             value_list = args[3].split()
             setattr(req_instance, args[2], parse_string(value_list[0]))
         storage.save()
-     
+
     def is_integer(x):
         """Checks if `x` is int.
         """
@@ -223,8 +223,8 @@ class HBNBCommand(cmd.Cmd):
         except (TypeError, ValueError):
             return False
         else:
-            return a == b   
-        
+            return a == b
+
     def parse_string(arg):
         """Parse `arg` to an `int`, `float` or `string`.
         """
