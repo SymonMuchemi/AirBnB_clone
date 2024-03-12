9f6f3870252fbd35d9351f49ccafb982510b2daa
SymonMuchemi <simonkenya846@gmail.com>
2024-03-12 18:03:01 +0300

    tested the user model

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,25 @@
+"""Testing the user module"""
+from models.user import User
+from unittest import TestCase
+
+
+class TestUser(TestCase):
+    """Checking the user model"""
+    user = User()
+    
+    def test_user_has_id(self):
+        """see if the user has an ID from base model"""
+        self.assertIsNotNone(self.user.id)
+
+    def test_user_has_no_email(self):
+        """user should not have an email address"""
+        self.assertEqual(self.user.email, "", "user has no email")
+
+    def test_user_has_no_password(self):
+        """user should not have a firstname"""
+        self.assertEqual(self.user.password, "", "no password")
+
+    def test_user_has_no_first_name(self):
+        """user should not have a firstname"""
+        self.assertEqual(self.user.first_name, "", "no first name")
+        
